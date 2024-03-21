2120283b05caf312bb7dcdca55f98d4fc5b784ce
Ethan <ethanfree23@gmail.com>
2024-03-21 17:23:42 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :facts
+  resources :locations
+  # Making routes point towards /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :users, :facts, :locations
+    end
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
